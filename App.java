33f92f80533f9ec88be98aa1bdbe1ef2f332c003
Sheesh-aint-pro <cedricechalar6@gmail.com>
2025-11-28 18:17:37 +0800

    Wala lng

--- App.java
+++ App.java
@@ -1,29 +1,21 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
 
-        ArrayList<String> names = new ArrayList<>();
-        ArrayList<Integer> age = new ArrayList<>();
-        ArrayList<String> address = new ArrayList<>();
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter Name: ");
-        String input_name = sc.nextLine();
-        names.add(input_name);
-
+        String name = sc.nextLine();
+        
         System.out.print("Enter Age: ");
-        int input_age = sc.nextInt();
-        age.add(input_age);
+        int age = sc.nextInt();
 
         sc.nextLine();
 
         System.out.print("Enter Address: ");
-        String input_adr = sc.nextLine();
-        address.add(input_adr);
+        String address = sc.nextLine();
 
-        System.out.println("Name: " + names + "\nAge: " + age + "\nAddress: " + address);
+        System.out.println("Name: " + name + "\nAge: " + age + "\nAddress: " + address);
     }
 }
